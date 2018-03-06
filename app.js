baa8ae5f779ada3e2b503455c74ad1251b0efa6b
Pavel Grigoryev <p.grigoryev@corp.mail.ru>
2018-03-06 12:37:14 +0300

    make some changes

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/searchProvider.xml', function (req, res) {
   res.sendFile(path.join(__dirname + '/searchProvider.xml'));
 });
 
+app.get('/searchProviderMail.xml', function (req, res) {
+  console.log(req.query.data);
+  res.sendFile(path.join(__dirname + '/searchProviderMail.xml'));
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log('Example app listening on port 5000!');
