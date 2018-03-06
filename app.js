293ec22e36c4b7b67b7bdc3ae7bf8c90d2fccfcd
Pavel Grigoryev <p.grigoryev@corp.mail.ru>
2018-03-06 12:22:00 +0300

    some fix

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/searchProvider.xml', function (req, res) {
+  console.log(req.query.data);
+  res.sendFile(path.join(__dirname + '/searchProvider.xml'));
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log('Example app listening on port 5000!');
